6f149ae6ef9ccd1e02cc75ec0253637637109ed1
MBing <bing.martin.photography@gmail.com>
2014-09-21 15:03:15 +0200

    Add extra views for better inner workings of ui router

--- app/scripts/ngMailCreator.js
+++ app/scripts/ngMailCreator.js
@@ -1,25 +1,27 @@
-'use strict';
-
 var mailCreator = angular.module('ngMailCreator', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'mm.foundation']);
 
 mailCreator
   .config(function ($stateProvider, $urlRouterProvider) {
+  	'use strict';
     $stateProvider
       .state('home', {
-        url: "/",
+        url: '/',
         views: {
-        	"left-nav": {
-        		templateUrl: 'partials/main.html',
+        	'left-nav': {
+        		templateUrl: 'partials/left-nav.html',
 		        controller: 'MainCtrl'
         	},
-        	"content": {
-        		templateUrl: 'partials/main.html',
+        	'content': {
+        		templateUrl: 'partials/content.html',
 		        controller: 'MainCtrl'
+        	},
+        	'mail@home': {
+        		templateUrl: 'partials/mail-preview.html',
+        		controller: 'MainCtrl'
         	}
         }
-        
       });
 
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise('/');
   })
 ;
