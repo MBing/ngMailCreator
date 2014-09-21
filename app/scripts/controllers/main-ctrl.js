b3f039b32e0ac75841dba831f770c5c2833e0717
MBing <bing.martin.photography@gmail.com>
2014-09-21 18:59:53 +0200

    Add views and adjust for better ui routing with sideview

--- app/scripts/controllers/main-ctrl.js
+++ app/scripts/controllers/main-ctrl.js
@@ -1,6 +1,18 @@
 mailCreator
   .controller('MainCtrl', function ($scope) {
     'use strict';
-    console.log($scope);
-    console.log('geladen');
+    $scope.allowDrop = function (ev) {
+        ev.preventDefault();
+    }
+
+    $scope.drag = function (ev) {
+        console.log('drag is started');
+        ev.dataTransfer.setData("text/html", ev.target.id);
+    }
+
+    $scope.drop = function (ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text/html");
+        ev.target.appendChild(document.getElementById(data));
+    }
   });
--- app/scripts/ngMailCreator.js
+++ app/scripts/ngMailCreator.js
@@ -1,4 +1,4 @@
-var mailCreator = angular.module('ngMailCreator', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'mm.foundation']);
+var mailCreator = angular.module('ngMailCreator', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'mm.foundation', 'ngDraggable']);
 
 mailCreator
   .config(function ($stateProvider, $urlRouterProvider) {
@@ -7,19 +7,33 @@ mailCreator
       .state('home', {
         url: '/',
         views: {
-        	'left-nav': {
-        		templateUrl: 'partials/left-nav.html',
-		        controller: 'MainCtrl'
+        	'main': {
+        		templateUrl: 'partials/main.html',
+        		controller: 'MainCtrl'
         	},
-        	'content': {
+        	'content@home': {
         		templateUrl: 'partials/content.html',
-		        controller: 'MainCtrl'
+        		controller: 'MainCtrl'
         	},
-        	'mail@home': {
+        	'left-side@home': {
+        		templateUrl: 'partials/left-nav.html',
+        		controller: 'MainCtrl'
+        	},
+        	'mail-preview@home': {
         		templateUrl: 'partials/mail-preview.html',
         		controller: 'MainCtrl'
         	}
         }
+      })
+      .state('about', {
+      	parent: 'home',
+      	url: '/about',
+      	views: {
+      		'content@home': {
+      			templateUrl: 'partials/about.html',
+      			controller: 'AboutCtrl'
+      		}
+      	}
       });
 
     $urlRouterProvider.otherwise('/');
